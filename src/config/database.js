619f03126bf8f9240db485ade79427c2212edd92
Harsh Singh <harshsingh127.0.0.1@gmail.com>
2025-09-17 17:22:10 +0530

    Hey

--- src/config/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-async function connectDB() {
-  await mongoose.connect(
-    "mongodb+srv://harshsingh6103:Harsh999@bbxbot.yya0c.mongodb.net/"
-  );
-}
-
-module.exports = connectDB;
